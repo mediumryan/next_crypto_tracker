abc3899ebc058b997651b978249ca289b046bce1
ryan <straightwind0726@gmail.com>
2024-10-21 11:49:51 +0900

    [Update] Tailwind 경로 에러 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   content: [
     './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
